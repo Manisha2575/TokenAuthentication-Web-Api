9af1d1dd9571f3922594e6579b4da27abf06f056
Manisha Pandey <manisha.pandey@ixc.com>
2017-11-14 17:41:52 +0530

    Add project files.

---
+++ webApiTokenAuthentication/MyAuthorizationServerProvider.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace webApiTokenAuthentication
+{
+     //static data
+    //Inherit OAuthAuthorizationService Provider
+    public class MyAuthorizationServerProvider: OAuthAuthorizationServerProvider
+    {
+
+
+        //Override method from OAuthAuthorizationServerProvider
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            //Validated the client
+            context.Validated();         
+        }
+
+
+
+        //Validate Credentials of user if valid generate sign token user can authorize access of server
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if( context.UserName == "admin" && context.Password ==   "admin")
+            {
+                 //If user n password is admin
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Manisha Pandey"));
+                context.Validated(identity);
+            }
+            else if(context.UserName== "user" && context.Password== "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Sapna Pandey"));
+                context.Validated(identity);
+            }
+            else
+            {
+                //set error if condition is false
+                context.SetError("invalid_grant", "Provided name is Incorrect");
+                return;
+            }
+        }
+    }
+}
---
+++ webApiTokenAuthentication/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(webApiTokenAuthentication.Startup))]
+
+namespace webApiTokenAuthentication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            //enable cors origin request
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            //Config AuthAuthorizationserver
+            var myProvider= new MyAuthorizationServerProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                //Allow insecure http
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = myProvider
+            };
+            //tell application to use oauthauthorizationServer
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            HttpConfiguration config = new HttpConfig10101010101010101010101010101010101010101010101010101010101010101010uration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
+//401 & 403 response status code for get authentication and authorization data 
+//401 => unauthenticated, it lacks valid authenticated credintial (request not applied lack authentication )
+//403 => authenticated but not authorized to request perform operation on given response (user can't see functionality of  admin)
